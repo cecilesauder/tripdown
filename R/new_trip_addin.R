4e2fce4dc46bba2e2f5fe5e48b77930e02e5c636
cecilesauder <cecile.sauder@gmail.com>
2018-06-27 19:43:10 +0200

    addin for new post

--- R/new_trip_addin.R
+++ R/new_trip_addin.R
@@ -8,24 +8,21 @@
 #' @export
 #' 
 new_trip_addin <- function() {
-  
-  # Our ui will be a simple gadget page, which
-  # simply displays the time in a 'UI' output.
-  ui <- miniUI::miniPage(
-    miniUI::gadgetTitleBar("New Trip"),
-    miniUI::miniContentPanel(
-      shiny::textInput("trip_name", label = "Trip Name (for the folder)", value = "NYC2018"),
-      shiny::textInput("title", label = "Trip Title", value = "New York 2018"),
-      shiny::fileInput("header", label = "Trip Header Image", buttonLabel = "Select image...")
+
+  ui <- miniPage(
+    gadgetTitleBar("New Trip"),
+    miniContentPanel(
+      textInput("trip_name", label = "Trip Name (for the folder)", value = "NYC2018"),
+      textInput("title", label = "Trip Title", value = "New York 2018"),
+      fileInput("header", label = "Trip Header Image", buttonLabel = "Select image...")
     )
   )
   
   server <- function(input, output, session) {
     
     
-    # Listen for 'done' events. When we're finished, we'll
-    # insert the current time, and then stop the gadget.
-    shiny::observeEvent(input$done, {
+    # Listen for 'done' events. 
+    observeEvent(input$done, {
       image <- input$header
       new_trip(trip_name = input$trip_name, 
                title = input$title, 
@@ -37,7 +34,7 @@ new_trip_addin <- function() {
   
   # We'll use a pane viwer, and set the minimum height at
   # 300px to ensure we get enough screen space to display the clock.
-  viewer <- shiny::paneViewer(300)
-  shiny::runGadget(ui, server, viewer = viewer)
+  viewer <- paneViewer(300)
+  runGadget(ui, server, viewer = viewer)
   
 }
---
+++ R/new_trip_post_addin.R
@@ -0,0 +1,43 @@
+#' new_trip_post_addin
+#'
+#' Addin to create a new trip in the blog
+#' @import shiny
+#' @import miniUI
+#' 
+#' 
+#' @export
+#' 
+new_trip_post_addin <- function() {
+  
+  ui <- miniPage(
+    gadgetTitleBar("New Trip"),
+    miniContentPanel(
+      list_trip <- path_file( dir_ls("content/", type = "directory") ),
+      selectInput("trip_name", label = "Select a trip :", choices = list_trip), 
+      textInput("title", label = "Post Title", value = "Brooklyn Heights"),
+      textInput("date", label = "Date", value = Sys.Date()),
+      textInput("author", label = "Author", value = "me")
+    )
+  )
+  
+  server <- function(input, output, session) {
+    
+    
+    # Listen for 'done' events. 
+    observeEvent(input$done, {
+      image <- input$header
+      new_trip_post(trip_name = input$trip_name, 
+                    date = input$date, 
+                    title = input$title,
+                    author = input$author)
+      stopApp()
+    })
+    
+  }
+  
+  # We'll use a pane viwer, and set the minimum height at
+  # 300px to ensure we get enough screen space to display the clock.
+  viewer <- paneViewer(300)
+  runGadget(ui, server, viewer = viewer)
+  
+}
