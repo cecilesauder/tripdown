d8a91501384fc9aa71128977a6929f6068d250f6
cecilesauder <cecile.sauder@gmail.com>
2018-07-02 22:32:03 +0200

    md ==> rmd function text_map

---
+++ R/text_map.R
@@ -0,0 +1,13 @@
+text_map <- function(lng, lat, place){
+  glue::glue(
+    "
+```{{r echo = FALSE}}
+library(leaflet)
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=lng, lat=lat, popup=place)
+m
+```
+"
+    )
+}
